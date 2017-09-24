ea9fc396e2ab26e88b0274ec709e70f24e8b6c93
SoumitraAgarwal <agarwalsoumitra1504@gmail.com>
2017-09-25 00:12:08 +0530

    Face alignment

--- Reshape.py
+++ Reshape.py
@@ -44,6 +44,38 @@ def correct_width(img, correction, start):
 		img = cv2.resize(img, (img.shape[0], img.shape[1] + 1)) 
 	return img
 
+
+def correctposx(img, correction):
+
+	if(correction < 0):
+
+		add = np.zeros((img.shape[0], 1, 3))
+		img = img[:, 0:img.shape[1] - 1]
+		img = np.hstack((add, img))
+		# shift left
+
+	else:
+		add = np.zeros((img.shape[0], 1, 3))
+		img = img[:, 1:img.shape[1]]
+		img = np.hstack((img, add))
+	return img
+
+
+def correctposy(img, correction):
+
+	if(correction < 0):
+
+		add = np.zeros((1,img.shape[1], 3))
+		img = img[0:img.shape[1] - 1, :]
+		img = np.vstack((add, img))
+		# shift down
+
+	else:
+		add = np.zeros((1,img.shape[1], 3))
+		img = img[ 1:img.shape[1], :]
+		img = np.vstack((img, add))
+	return img
+
 base 		= 'Pictures/'
 positions 	= os.listdir(base)
 detector 	= dlib.get_frontal_face_detector()
@@ -77,6 +109,7 @@ l_eyes  = [116, 66]
 r_eyes	= [143, 63]
 mouth 	= [130, 98]
 nose  	= [126, 76]
+lr_mean = [(l_eyes[0] + r_eyes[0])/2.0 ,(l_eyes[1] + r_eyes[1])/2.0]
 
 for image in images:
 	img = cv2.imread(base + image)
@@ -99,23 +132,73 @@ for image in images:
 		s_mouth = [int(0.5*x) for x in s_mouth]
 		s_nose 	= [int(0.5*x) for x in s_nose]
 
-		correction = r_eyes[0] - l_eyes[0] - sr_eyes[0] + sl_eyes[0]
+		e_mean 	= [(sl_eyes[0] + sr_eyes[0])/2.0, (sl_eyes[1] + sr_eyes[1])/2.0]
 
+		correctionx = e_mean[0] - lr_mean[0]  
+		correctiony = e_mean[1] - lr_mean[1]
+		
+		cv2.imwrite("Worked/" + image, img)			
 		start 	= 0
-		while(abs(correction) >= 1):
+		while(abs(correctionx) > 1):
 			
-			print(correction)
+			print(correctionx)	
+			start += 1
+			img 	= correctposx(img, correctionx) 
+			cv2.imwrite("Worked/" + image, img)
 			
-			img 	= correct_width(img, correction, start) 
-			start 	= 1 - start 
 			sl_eyes = [0, 0]
 			sr_eyes	= [0, 0]
+			img 	= cv2.imread("Worked/" + image)
+			detsp 	= detector(img, 1)
+			for k1, d1 in enumerate(detsp):
+				sl_eyes, sr_eyes, s_mouth, s_nose = get_params(img, d1, sl_eyes, sr_eyes, s_mouth, s_nose)
+				e_mean 	= [(sl_eyes[0] + sr_eyes[0])/2.0, (sl_eyes[1] + sr_eyes[1])/2.0]
+				correctionx = e_mean[0] - lr_mean[0]  
+				correctiony = e_mean[1] - lr_mean[1]
 
-			sl_eyes, sr_eyes, s_mouth, s_nose = get_params(img, d, sl_eyes, sr_eyes, s_mouth, s_nose)
 
-			correction = r_eyes[0] - l_eyes[0] - sr_eyes[0] + sl_eyes[0]
-			# print(l_eyes[0] - sl_eyes[0]	, 	l_eyes[1] - sl_eyes[1])
-			# print(r_eyes[0] - sr_eyes[0]	,	r_eyes[1] - sr_eyes[1])
-			# print(mouth[0]- s_mouth[0]	,	mouth[1]- s_mouth[1])
-			# print(nose[0] - s_nose[0]	, 	nose[1] - s_nose[1])
-		cv2.imwrite("Worked/" + image, img)
+for image in images:
+
+	print("Working for " + image)
+	img = cv2.imread("Worked/" + image)
+	dets = detector(img, 1)
+	for k, d in enumerate(dets):
+		
+		# Get the landmarks/parts for the face in box d.
+		shape = predictor(img, d)
+		
+		sl_eyes = [0, 0]
+		sr_eyes	= [0, 0]
+		s_mouth	= [0, 0]
+		s_nose 	= [0, 0]
+
+		sl_eyes, sr_eyes, s_mouth, s_nose = get_params(img, d, sl_eyes, sr_eyes, s_mouth, s_nose)
+
+		# Average calculation
+		s_mouth = [int(0.5*x) for x in s_mouth]
+		s_nose 	= [int(0.5*x) for x in s_nose]
+
+		e_mean 	= [(sl_eyes[0] + sr_eyes[0])/2.0, (sl_eyes[1] + sr_eyes[1])/2.0]
+
+		correctionx = e_mean[0] - lr_mean[0]  
+		correctiony = e_mean[1] - lr_mean[1]
+		
+		cv2.imwrite("Worked/" + image, img)			
+		start 	= 0
+		while(abs(correctiony) > 2):
+			
+			print(correctiony)	
+			start += 1
+			img 	= correctposy(img, correctiony) 
+			cv2.imwrite("Worked/" + image, img)
+			
+			sl_eyes = [0, 0]
+			sr_eyes	= [0, 0]
+			img 	= cv2.imread("Worked/" + image)
+			detsp 	= detector(img, 1)
+			for k1, d1 in enumerate(detsp):
+				sl_eyes, sr_eyes, s_mouth, s_nose = get_params(img, d1, sl_eyes, sr_eyes, s_mouth, s_nose)
+				e_mean 	= [(sl_eyes[0] + sr_eyes[0])/2.0, (sl_eyes[1] + sr_eyes[1])/2.0]
+				correctionx = e_mean[0] - lr_mean[0]  
+				correctiony = e_mean[1] - lr_mean[1]
+			
