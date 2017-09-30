904c018c6529bad7e781495c5d8da6b639a7f1e4
SoumitraAgarwal <agarwalsoumitra1504@gmail.com>
2017-10-01 04:42:40 +0530

    Facial alignment

--- Reshape.py
+++ Reshape.py
@@ -104,94 +104,128 @@ print(l_eyes, r_eyes, mouth, nose)
 
 lr_mean = [(l_eyes[0] + r_eyes[0])/2.0 ,(l_eyes[1] + r_eyes[1])/2.0]
 
-for image in images:
-	img = cv2.imread(base + image)
-	dets = detector(img, 1)
-	for k, d in enumerate(dets):
+# for image in images:
+# 	img = cv2.imread(base + image)
+# 	dets = detector(img, 1)
+# 	for k, d in enumerate(dets):
 		
-		print("Working for " + image)
+# 		print("Working for " + image)
 		
-		# Get the landmarks/parts for the face in box d.
-		shape = predictor(img, d)
+# 		# Get the landmarks/parts for the face in box d.
+# 		shape = predictor(img, d)
 		
-		sl_eyes = [0, 0]
-		sr_eyes	= [0, 0]
-		s_mouth	= [0, 0]
-		s_nose 	= [0, 0]
+# 		sl_eyes = [0, 0]
+# 		sr_eyes	= [0, 0]
+# 		s_mouth	= [0, 0]
+# 		s_nose 	= [0, 0]
 
-		sl_eyes, sr_eyes, s_mouth, s_nose = get_params(img, d, sl_eyes, sr_eyes, s_mouth, s_nose)
+# 		sl_eyes, sr_eyes, s_mouth, s_nose = get_params(img, d, sl_eyes, sr_eyes, s_mouth, s_nose)
 
-		# Average calculation
-		s_mouth = [int(0.5*x) for x in s_mouth]
-		s_nose 	= [int(0.5*x) for x in s_nose]
+# 		# Average calculation
+# 		s_mouth = [int(0.5*x) for x in s_mouth]
+# 		s_nose 	= [int(0.5*x) for x in s_nose]
 
-		e_mean 	= [(sl_eyes[0] + sr_eyes[0])/2.0, (sl_eyes[1] + sr_eyes[1])/2.0]
+# 		e_mean 	= [(sl_eyes[0] + sr_eyes[0])/2.0, (sl_eyes[1] + sr_eyes[1])/2.0]
 
-		correctionx = e_mean[0] - lr_mean[0]  
-		correctiony = e_mean[1] - lr_mean[1]
+# 		correctionx = e_mean[0] - lr_mean[0]  
+# 		correctiony = e_mean[1] - lr_mean[1]
 		
-		cv2.imwrite("Worked/" + image, img)			
-		start 	= 0
-		while(abs(correctionx) > 1):
+# 		cv2.imwrite("../Pictures/Worked/" + image, img)			
+# 		start 	= 0
+# 		while(abs(correctionx) > 1):
 			
-			print(correctionx)	
-			start += 1
-			img 	= correctposx(img, correctionx) 
-			cv2.imwrite("Worked/" + image, img)
+# 			print(correctionx)	
+# 			start += 1
+# 			img 	= correctposx(img, correctionx) 
+# 			cv2.imwrite("../Pictures/Worked/" + image, img)
 			
-			sl_eyes = [0, 0]
-			sr_eyes	= [0, 0]
-			img 	= cv2.imread("Worked/" + image)
-			detsp 	= detector(img, 1)
-			for k1, d1 in enumerate(detsp):
-				sl_eyes, sr_eyes, s_mouth, s_nose = get_params(img, d1, sl_eyes, sr_eyes, s_mouth, s_nose)
-				e_mean 	= [(sl_eyes[0] + sr_eyes[0])/2.0, (sl_eyes[1] + sr_eyes[1])/2.0]
-				correctionx = e_mean[0] - lr_mean[0]  
-				correctiony = e_mean[1] - lr_mean[1]
+# 			sl_eyes = [0, 0]
+# 			sr_eyes	= [0, 0]
+# 			img 	= cv2.imread("../Pictures/Worked/" + image)
+# 			detsp 	= detector(img, 1)
+# 			for k1, d1 in enumerate(detsp):
+# 				sl_eyes, sr_eyes, s_mouth, s_nose = get_params(img, d1, sl_eyes, sr_eyes, s_mouth, s_nose)
+# 				e_mean 	= [(sl_eyes[0] + sr_eyes[0])/2.0, (sl_eyes[1] + sr_eyes[1])/2.0]
+# 				correctionx = e_mean[0] - lr_mean[0]  
+# 				correctiony = e_mean[1] - lr_mean[1]
 
+
+# for image in images:
+
+# 	print("Working for " + image)
+# 	img = cv2.imread("../Pictures/Worked/" + image)
+# 	dets = detector(img, 1)
+# 	for k, d in enumerate(dets):
+		
+# 		# Get the landmarks/parts for the face in box d.
+# 		shape = predictor(img, d)
+		
+# 		sl_eyes = [0, 0]
+# 		sr_eyes	= [0, 0]
+# 		s_mouth	= [0, 0]
+# 		s_nose 	= [0, 0]
+
+# 		sl_eyes, sr_eyes, s_mouth, s_nose = get_params(img, d, sl_eyes, sr_eyes, s_mouth, s_nose)
+
+# 		# Average calculation
+# 		s_mouth = [int(0.5*x) for x in s_mouth]
+# 		s_nose 	= [int(0.5*x) for x in s_nose]
+
+# 		e_mean 	= [(sl_eyes[0] + sr_eyes[0])/2.0, (sl_eyes[1] + sr_eyes[1])/2.0]
+
+# 		correctionx = e_mean[0] - lr_mean[0]  
+# 		correctiony = e_mean[1] - lr_mean[1]
+		
+# 		cv2.imwrite("../Pictures/Worked/" + image, img)			
+# 		start 	= 0
+# 		while(abs(correctiony) > 1):
+			
+# 			print(correctiony)	
+# 			start += 1
+# 			img 	= correctposy(img, correctiony) 
+# 			cv2.imwrite("../Pictures/../Pictures/Worked/" + image, img)
+			
+# 			sl_eyes = [0, 0]
+# 			sr_eyes	= [0, 0]
+# 			img 	= cv2.imread("../Pictures/Worked/" + image)
+# 			detsp 	= detector(img, 1)
+# 			for k1, d1 in enumerate(detsp):
+# 				sl_eyes, sr_eyes, s_mouth, s_nose = get_params(img, d1, sl_eyes, sr_eyes, s_mouth, s_nose)
+# 				e_mean 	= [(sl_eyes[0] + sr_eyes[0])/2.0, (sl_eyes[1] + sr_eyes[1])/2.0]
+# 				correctionx = e_mean[0] - lr_mean[0]  
+# 				correctiony = e_mean[1] - lr_mean[1]
+# 			
+
+# l_eyes = [1331, 601];
+# r_eyes = [1523, 577];
+# mouth  = [1432, 833];
+# nose   = [1406, 684];
 
 for image in images:
-
 	print("Working for " + image)
-	img = cv2.imread("Worked/" + image)
-	dets = detector(img, 1)
+	img 		= cv2.imread(base + image)
+	dets 		= detector(img, 1)
+	rows,cols,ch = img.shape
 	for k, d in enumerate(dets):
 		
 		# Get the landmarks/parts for the face in box d.
 		shape = predictor(img, d)
-		
 		sl_eyes = [0, 0]
 		sr_eyes	= [0, 0]
 		s_mouth	= [0, 0]
 		s_nose 	= [0, 0]
 
 		sl_eyes, sr_eyes, s_mouth, s_nose = get_params(img, d, sl_eyes, sr_eyes, s_mouth, s_nose)
-
-		# Average calculation
 		s_mouth = [int(0.5*x) for x in s_mouth]
-		s_nose 	= [int(0.5*x) for x in s_nose]
+		pts1 = np.float32([sl_eyes, sr_eyes, s_mouth])
+		pts2 = np.float32([l_eyes, r_eyes, mouth])
 
-		e_mean 	= [(sl_eyes[0] + sr_eyes[0])/2.0, (sl_eyes[1] + sr_eyes[1])/2.0]
-
-		correctionx = e_mean[0] - lr_mean[0]  
-		correctiony = e_mean[1] - lr_mean[1]
+		# for i in range(68):
+		# 	# cv2.circle(img,(shape.part(i).x,shape.part(i).y),4,(0,0,255))
+		# 	cv2.circle(img,(s_mouth[0],s_mouth[1]),4,(0,0,255))
 		
-		cv2.imwrite("Worked/" + image, img)			
-		start 	= 0
-		while(abs(correctiony) > 1):
-			
-			print(correctiony)	
-			start += 1
-			img 	= correctposy(img, correctiony) 
-			cv2.imwrite("Worked/" + image, img)
-			
-			sl_eyes = [0, 0]
-			sr_eyes	= [0, 0]
-			img 	= cv2.imread("Worked/" + image)
-			detsp 	= detector(img, 1)
-			for k1, d1 in enumerate(detsp):
-				sl_eyes, sr_eyes, s_mouth, s_nose = get_params(img, d1, sl_eyes, sr_eyes, s_mouth, s_nose)
-				e_mean 	= [(sl_eyes[0] + sr_eyes[0])/2.0, (sl_eyes[1] + sr_eyes[1])/2.0]
-				correctionx = e_mean[0] - lr_mean[0]  
-				correctiony = e_mean[1] - lr_mean[1]
-			
+		print(sl_eyes, sr_eyes, s_mouth)
+		print(l_eyes, r_eyes, mouth)	
+		M = cv2.getAffineTransform(pts1,pts2)
+		dst = cv2.warpAffine(img,M,(cols,rows))	
+		cv2.imwrite("Worked/" + image, dst)
